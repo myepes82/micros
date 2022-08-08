67235b74f479b9eff47a1931ef94bdec0176183f
Ghost Shell <marlondevjs@gmail.com>
2022-08-08 16:37:36 -0500

    eureka implemented

---
+++ registry-service/src/test/java/com/eureka/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
