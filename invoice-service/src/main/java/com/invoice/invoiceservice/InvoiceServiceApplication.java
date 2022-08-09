adda6c9a050f18b9f00436cdf9232f58219456fe
Ghost Shell <marlondevjs@gmail.com>
2022-08-09 10:08:05 -0500

    topic retry config updated

--- invoice-service/src/main/java/com/invoice/invoiceservice/InvoiceServiceApplication.java
+++ invoice-service/src/main/java/com/invoice/invoiceservice/InvoiceServiceApplication.java
@@ -3,10 +3,8 @@ package com.invoice.invoiceservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.kafka.annotation.EnableKafka;
 
 
-@EnableKafka
 @EnableEurekaClient
 @SpringBootApplication
 public class InvoiceServiceApplication {
